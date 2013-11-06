6a8fb0730dd5645801e715d222ab19be7b118ccc
Kieran <kieranajp@me.com>
2013-11-06 12:10:30 +0000

    Added Twig's dump() extension
    Fixes #10

--- TwigExtension.php
+++ TwigExtension.php
@@ -47,6 +47,7 @@ class TwigExtension extends \Twig_Extension
             new \Twig_SimpleFunction('urlFor', array($this, 'urlFor')),
             new \Twig_SimpleFunction('baseUrl', array($this, 'base')),
             new \Twig_SimpleFunction('siteUrl', array($this, 'site')),
+            new \Twig_SimpleFunction('dump', array($this, 'dump')),
         );
     }
 
@@ -69,5 +70,10 @@ class TwigExtension extends \Twig_Extension
             $uri .= $req->getRootUri();
         }
         return $uri;
+    }
+    
+    public function dump($var, $appName = 'default')
+    {
+        return var_dump($var);
     }
 }

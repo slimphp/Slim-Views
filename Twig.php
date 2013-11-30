ab996e6a7ff6198970e14135e06410fa7cce7232
Ilko <kachar136@gmail.com>
2013-11-30 19:21:14 +0200

    Fix declaration incompatibility with Slim\View
    After this commit the `Slim\Views\Twig::render()` declaration is not compatible with the `Slim\View::render()`
    
    https://github.com/codeguy/Slim/commit/8e3b1fcc303eb9964c08a62404bc415ccb1189a6 
    
    `Strict Standards: Declaration of Slim\Views\Twig::render() should be compatible with Slim\View::render($template, $data = NULL) in /app/vendor/slim/views/Slim/Views/Twig.php on line 46`

--- Twig.php
+++ Twig.php
@@ -80,7 +80,7 @@ class Twig extends \Slim\View
      * @param   string $template The path to the Twig template, relative to the Twig templates directory.
      * @return  void
      */
-    public function render($template)
+    public function render($template, $data = null)
     {
         $env = $this->getInstance();
         $parser = $env->loadTemplate($template);

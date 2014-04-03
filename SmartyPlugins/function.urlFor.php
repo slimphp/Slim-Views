f0a05ea0b654218e40de692ed7cddcad1cf4b828
silentworks <a.smith@silentworks.co.uk>
2014-04-03 17:31:10 +0100

    Update views with new version number and updated docs.

--- SmartyPlugins/function.urlFor.php
+++ SmartyPlugins/function.urlFor.php
@@ -1,13 +1,13 @@
 <?php
-/*
+/**
  * Smarty plugin
  * -------------------------------------------------------------
  * File:     function.urlFor.php
  * Type:     function
  * Name:     urlFor
  * Purpose:  outputs url for a function with the defined name method
- * version   0.1.0
- * package   SlimViews
+ * @version   0.1.2
+ * @package   SlimViews
  * -------------------------------------------------------------
  */
 function smarty_function_urlFor($params, $template)
